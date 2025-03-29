ec719f405b0c5fa5ef0217407c68f57b5ccba1e5
William Lin <william.linn02@gmail.com>
2025-03-29 16:37:10 -0500

    adding search page

---
+++ preggo-app/app/search/components/Map.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import "leaflet/dist/leaflet.css";
+import { useEffect, useState } from "react";
+
+// Dynamically import Leaflet to avoid SSR issues
+let L: any;
+if (typeof window !== "undefined") {
+  L = require("leaflet");
+}
+
+// Fix default marker icon issue in Leaflet
+if (L) {
+  delete L.Icon.Default.prototype._getIconUrl;
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+    iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+  });
+}
+
+// Dynamically import React-Leaflet components
+const MapContainer = dynamic(() => import("react-leaflet").then((mod) => mod.MapContainer), { ssr: false });
+const TileLayer = dynamic(() => import("react-leaflet").then((mod) => mod.TileLayer), { ssr: false });
+const Marker = dynamic(() => import("react-leaflet").then((mod) => mod.Marker), { ssr: false });
+const Popup = dynamic(() => import("react-leaflet").then((mod) => mod.Popup), { ssr: false });
+
+const Map = () => {
+  const [currentLocation, setCurrentLocation] = useState<[number, number] | null>(null);
+
+  useEffect(() => {
+    // Get the user's current location
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setCurrentLocation([latitude, longitude]);
+        },
+        (error) => {
+          console.error("Error getting location:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
+  return (
+    <MapContainer
+      center={currentLocation || [37.7749, -122.4194]} // Default to San Francisco if location is not available
+      zoom={13}
+      style={{ height: "500px", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {currentLocation && (
+        <Marker position={currentLocation}>
+          <Popup>Your Current Location</Popup>
+        </Marker>
+      )}
+    </MapContainer>
+  );
+};
+
+export default Map;
