0ff483e2ede6a0b2d16fd54b7d1b92a1772285db
Bennett Lincoln <bennettlincoln2@gmail.com>
2025-03-29 16:30:43 -0500

    added some pages

---
+++ preggo-app/app/patient_signin/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen p-8">
+        <h1 className="text-3xl font-bold mb-4">About Us</h1>
+        <p className="text-lg text-center max-w-2xl">
+          Patient Sign In
+        </p>
+      </div>
+    );
+  }
